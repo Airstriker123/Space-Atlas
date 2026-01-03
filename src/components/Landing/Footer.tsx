82c895d8f5e7382439daab6543b49ae851d2eb04
Airstriker <114228623+Airstriker123@users.noreply.github.com>
2026-01-03 23:04:16 +1100

    fixed mobile issues

--- src/components/Landing/Footer.tsx
+++ src/components/Landing/Footer.tsx
@@ -1,43 +1,36 @@
-export default function Footer()
-{
+export default function Footer() {
     return (
-        <footer className="flex flex-col h-[364px] items-start gap-12 px-16 py-[120px] relative self-stretch w-full bg-transparent">
-            <div className="flex items-start gap-12 relative self-stretch w-full flex-[0_0_auto] mb-[-55.00px]">
-                <div className="flex-col min-w-[324px] items-start gap-4 pt-6 pb-0 px-0 flex-1 grow border-t [border-top-style:solid] border-white relative flex">
-                    <div className="relative w-fit mt-[-1.00px] [font-family:'Inter',Helvetica] font-bold text-white text-2xl tracking-[-0.48px] leading-6 whitespace-nowrap">
+        <footer className="flex flex-col items-start gap-12 px-6 md:px-16 py-16 w-full">
+            <div className="flex flex-col md:flex-row gap-12 w-full">
+
+                <div className="flex flex-col gap-4 pt-6 border-t border-white w-full">
+                    <h3 className="font-bold text-white text-2xl">
                         inspiration
-                    </div>
-
-                    <p className="relative flex items-center justify-center self-stretch [font-family:'Inter',Helvetica] font-medium text-white text-base tracking-[-0.08px] leading-[23.2px]">
-                        I was inspired by space, futuristic design, and sci‑fi games. My
-                        project lets people feel like they’re exploring space.
+                    </h3>
+                    <p className="text-white text-base">
+                        I was inspired by space, futuristic design, and sci‑fi games. My project lets people feel like they’re exploring space.
                     </p>
                 </div>
 
-                <div className="flex-col min-w-[324px] items-start gap-4 pt-6 pb-0 px-0 flex-1 grow border-t [border-top-style:solid] border-white relative flex">
-                    <div className="relative w-fit mt-[-1.00px] [font-family:'Inter',Helvetica] font-bold text-white text-2xl tracking-[-0.48px] leading-6 whitespace-nowrap">
+                <div className="flex flex-col gap-4 pt-6 border-t border-white w-full">
+                    <h3 className="font-bold text-white text-2xl">
                         Learning experience
-                    </div>
-
-                    <p className="relative flex items-center justify-center self-stretch [font-family:'Inter',Helvetica] font-medium text-white text-base tracking-[-0.08px] leading-[23.2px]">
-                        This project allowed me to further improve my web development
-                        skills. By learning to use multimedia JavaScript libraries such
-                        as threeJS, GSAP I was able to improve my knowledge of we
-                        development.
+                    </h3>
+                    <p className="text-white text-base">
+                        This project allowed me to further improve my web development skills. By learning to use multimedia JavaScript libraries such as threeJS, GSAP I was able to improve my knowledge of we development.
                     </p>
                 </div>
 
-                <div className="flex-col min-w-[324px] items-start gap-4 pt-6 pb-0 px-0 flex-1 grow border-t [border-top-style:solid] border-white relative flex">
-                    <div className="relative w-fit mt-[-1.00px] [font-family:'Inter',Helvetica] font-bold text-white text-2xl tracking-[-0.48px] leading-6 whitespace-nowrap">
+                <div className="flex flex-col gap-4 pt-6 border-t border-white w-full">
+                    <h3 className="font-bold text-white text-2xl">
                         Time Usage
-                    </div>
-
-                    <p className="relative flex items-center justify-center self-stretch [font-family:'Inter',Helvetica] font-medium text-white text-base tracking-[-0.08px] leading-[23.2px]">
-                        This project took me about 1 month to create. I spend most of my
-                        school holidays working on it.
+                    </h3>
+                    <p className="text-white text-base">
+                        This project took me about 1 month to create. I spend most of my school holidays working on it.
                     </p>
                 </div>
+
             </div>
         </footer>
-    )
+    );
 }
