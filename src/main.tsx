f38d3dc79523093e44ecc3585e94902df3282540
Airstriker <114228623+Airstriker123@users.noreply.github.com>
2026-01-18 01:52:16 +1100

    make pwa (mobile)
    added support for other devices

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { registerSW } from 'virtual:pwa-register'; //import pwa register
+
+// allow service worker for pwa
+const updateSW = registerSW({
+    immediate: true,
+    onNeedRefresh() {
+        // Optional: Show a "New version available" toast
+        // If you want it to be truly automatic:
+        updateSW(true);
+    },
+    onOfflineReady() {
+        console.log('Space Atlas is ready for offline star-gazing!');
+    },
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path';
+import { VitePWA } from 'vite-plugin-pwa';
+import manifest from './manifest.json';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+      VitePWA({
+          //pwa config
+          manifest,
+          includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+          // switch to "true" to enable sw on development
+          devOptions: { enabled: true }, //allow pwa features on development server
+          registerType: 'autoUpdate',
+          workbox: { globPatterns: ['**/*.{js,css,html}', '**/*.{svg,png,jpg,gif}'] },
+      }),
       react(),
       tailwindcss(),
   ],
