eaa3a4722d519fe00fa4fe5f9aab390453ae0c5b
Amit Singh <114228623+Airstriker123@users.noreply.github.com>
2026-06-01 15:53:41 +1000

    Update main.tsx
    Signed-off-by: Amit Singh <114228623+Airstriker123@users.noreply.github.com>

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
-import { registerSW } from 'virtual:pwa-register'; //import pwa register
-
-
-registerSW({
-    immediate: true
-});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
