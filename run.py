f6553482d40c8a44559cabeb97987600a955b2ad
Amit Singh <114228623+Airstriker123@users.noreply.github.com>
2026-06-01 18:24:20 +1000

    Update run.py
    Signed-off-by: Amit Singh <114228623+Airstriker123@users.noreply.github.com>

--- run.py
+++ run.py
@@ -47,35 +47,12 @@ def client_setup(choice):
         print('Running client (development)...')
         os.system("npm run dev")
 
-    elif choice == "2":
+    if choice == "2":
         print('Building and Previewing (production mode)...')
         os.system("npm run build && npm run preview")
-
-    elif choice == "3":
-        def spinner(text):
-             for frame in itertools.cycle(["/", "-", "\\", "|"]):
-                if done:
-                 break
-                print(f"\r{text} {frame}", end="", flush=True)
-                time.sleep(0.1)
-
-        print('--- DEPLOYING TO GITHUB PAGES ---')
-        os.system("npm run build")
-
-
-        with open("build/.nojekyll", "w") as f:
-            f.write("")
-
-
-        done = False
-        t = threading.Thread(target=spinner, args=("Pushing changes",))
-        t.start()
-        os.system("npx gh-pages -d build")
-
-        done = True
-        t.join()
-        print("\rPushing changes ✔")
-        input("Deployment complete.")
+    else:
+        input("invalid input")
+        pass
 
 if __name__ == "__main__":
     try:
@@ -84,7 +61,6 @@ if __name__ == "__main__":
         print("---------------------------")
         print("[1] Run Development (Real-time editing)")
         print("[2] Run Production (Build and test locally)")
-        print("[3] Run Deploy  (ghp)")
 
         user_choice = input("\nEnter option: ")
         client_setup(user_choice)
